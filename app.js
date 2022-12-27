23db1714f22836ec0c7c0060a50969ddd606cea7
Cyrus Olinger <cyrusolinger@gmail.com>
2022-12-27 18:51:45 -0500

    Create function to add number to display based on button click

--- app.js
+++ app.js
@@ -20,3 +20,43 @@ function operate(operator, a, b) {
             break;
     }
 }
+
+const buttons = document.querySelector('.buttons')
+let screenCurr = document.querySelector('.screen-current')
+let screenLast = document.querySelector('.screen-last')
+let current = 0
+
+
+// Listener for button press
+buttons.addEventListener("click", function(event) {
+    let last = current
+    const operator = event.target.dataset.
+key
+    // if (current === 'clear') {
+    // } else if (current === 'delete') {
+    //     return
+    // } else if (current === 'decimal') {
+    //     return
+    // }
+     if (operator.startsWith("operator")) {
+        screenLast.textContent = `${screenCurr.textContent} ${event.target.textContent}`
+        current = 0
+    }
+    //  else if (current === 'equals') {
+    //     console.log('=')
+    // }
+     else if (!isNaN(event.target.textContent)) {
+        current = event.target.dataset.key
+        screenCurr.textContent += event.target.dataset.key
+    }
+        
+    // IF delete
+    // IF clear
+    // IF operator
+        // UPDATE screen-last with screen-current value and operator)
+    // IF number
+    // IF equals
+    // IF decimal
+
+})
+
